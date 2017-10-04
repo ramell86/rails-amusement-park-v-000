4c8f08aee8b1e53d49e6c6a18902b8451cf9b706
Andrew <github email address>
2017-10-04 15:15:27 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,5 @@
 class Ride < ActiveRecord::Base
   # write associations here
+  belongs_to :attraction
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  
 end
