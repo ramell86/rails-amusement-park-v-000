2966a446dd0c12b91a12773a96953513d0f44c32
Andrew <github email address>
2017-10-05 18:05:17 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete :name
+    session.delete :user_id
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ root 'users#home'
  
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   delete '/logout', to: 'sessions#destroy'
   # get 'users/:id', to: 'users#show'
 
