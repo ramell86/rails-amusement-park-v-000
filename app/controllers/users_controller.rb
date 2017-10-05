1a2fdcc556aab2219b35da24e9fcbced290a82f5
Andrew <github email address>
2017-10-04 23:59:06 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
 
-    # binding.pry
   end
 
   def create
     @user = User.new(user_params)
-
-    @user.save
-
-    redirect_to users_path(@user)
+     if @user.save
+    
+        session[:user_id] = @user.id
+      redirect_to @user
+     else 
+        render 'new'
+     end
   end
   def show
-    @user = User.find_by(id: params[:id])
+     @user = User.find_by(id: params[:id])
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
 root 'users#home'
-  resource :users
-  get 'users/:id', to: 'users#show'
 
+  resources :users
+  resources :sessions
+  # get 'users/:id', to: 'users#show'
 
 
 end
