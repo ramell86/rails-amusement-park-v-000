f8bd9fcf9b0322ff93d4780d3c886d01c3d97a2b
Andrew <github email address>
2017-10-06 16:47:55 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,6 +11,32 @@ class AttractionsController < ApplicationController
     
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+  
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+  def edit 
+    @attraction = Attraction.find_by_id(params[:id])
+    
+  end
+  def update
+    @attraction = Attraction.find_by_id(params[:id])
+    @attraction.update(attraction_params)
+    
+    redirect_to attraction_path
+  end
+
+  private
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+
+  end
+
 
 
 end
